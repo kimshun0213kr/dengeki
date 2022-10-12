0357e21fa7de804c4209c0faa067090c9ee65088
kimshunKR <96727208+kimshun0213kr@users.noreply.github.com>
2022-10-13 01:33:43 +0900

    Add files via upload

--- assets/js/latest.js
+++ assets/js/latest.js
@@ -1,7 +1,7 @@
 let title01 = 'はじめまして！'
 let year01 = '2022'
 let month01 = '10'
-let day1 = '12'
+let day1 = '14'
 let writer01 = 'ベレト'
 let tag01 = 'きつねからのお知らせ'
 
@@ -44,7 +44,7 @@ document.getElementById('day1').innerHTML = day1;
 let w01 = document.getElementById('writer1').innerHTML;
 document.getElementById('writer1').innerHTML = '<a href="writer/'+writersearch(writer01)+'/home.html" target="_parent">'+writer01+'</a>';
 let ta01 = document.getElementById('tag1').innerHTML;
-document.getElementById('tag1').innerHTML = '<a href="'+tagsearch(tag01)+'/'+tagsearch(tag01)+'.html" target="_parent">'+tag01+'</a>';
+document.getElementById('tag1').innerHTML = '<a href="tag/'+tagsearch(tag01)+'/'+tagsearch(tag01)+'.html" target="_parent">'+tag01+'</a>';
 
 let t02 = document.getElementById('No02').innerHTML;
 document.getElementById('No02').innerHTML = '<a href="'+year02+'/'+month02+'/'+month02+day2+'.html"><h1>'+title02+'</h1></a>';
@@ -57,7 +57,7 @@ document.getElementById('day2').innerHTML = day2;
 let w02 = document.getElementById('writer2').innerHTML;
 document.getElementById('writer2').innerHTML = '<a href="writer/'+writersearch(writer02)+'/home.html">'+writer02+'</a>';
 let tag2 = document.getElementById('tag2').innerHTML;
-document.getElementById('tag2').innerHTML = '<a href="'+tagsearch(tag02)+'/'+tagsearch(tag02)+'.html">'+tag02+'</a>';
+document.getElementById('tag2').innerHTML = '<a href="tag/'+tagsearch(tag02)+'/'+tagsearch(tag02)+'.html">'+tag02+'</a>';
 
 let t03 = document.getElementById('No03').innerHTML;
 document.getElementById('No03').innerHTML = '<a href="'+year03+'/'+month03+'/'+month03+day3+'.html"><h1>'+title03+'</h1></a>';
@@ -70,7 +70,7 @@ document.getElementById('day3').innerHTML = day3;
 let w03 = document.getElementById('writer3').innerHTML;
 document.getElementById('writer3').innerHTML = '<a href="writer/'+writersearch(writer03)+'/home.html">'+writer03+'</a>';
 let tag3 = document.getElementById('tag3').innerHTML;
-document.getElementById('tag3').innerHTML = '<a href="'+tagsearch(tag03)+'/'+tagsearch(tag03)+'.html">'+tag03+'</a>';
+document.getElementById('tag3').innerHTML = '<a href="tag/'+tagsearch(tag03)+'/'+tagsearch(tag03)+'.html">'+tag03+'</a>';
 
 let t04 = document.getElementById('No4').innerHTML;
 document.getElementById('No04').innerHTML = '<a href="'+year04+'/'+month04+'/'+month04+day4+'.html"><h1>'+title04+'</h1></a>';
@@ -83,7 +83,7 @@ document.getElementById('day4').innerHTML = day4;
 let w04 = document.getElementById('writer4').innerHTML;
 document.getElementById('writer4').innerHTML = '<a href="writer/'+writersearch(writer04)+'/home.html">'+writer04+'</a>';
 let tag4 = document.getElementById('tag4').innerHTML;
-document.getElementById('tag4').innerHTML = '<a href="'+tagsearch(tag04)+'/'+tagsearch(tag04)+'.html">'+tag04+'</a>';
+document.getElementById('tag4').innerHTML = '<a href="tag/'+tagsearch(tag04)+'/'+tagsearch(tag04)+'.html">'+tag04+'</a>';
 
 let t05 = document.getElementById('No05').innerHTML;
 document.getElementById('No05').innerHTML = '<a href="'+year05+'/'+month05+'/'+month05+day5+'.html"><h1>'+title05+'</h1></a>';
@@ -96,7 +96,7 @@ document.getElementById('day5').innerHTML = day5;
 let w05 = document.getElementById('writer5').innerHTML;
 document.getElementById('writer5').innerHTML = '<a href="writer/'+writersearch(writer05)+'/home.html">'+writer05+'</a>';
 let ta05 = document.getElementById('tag5').innerHTML;
-document.getElementById('tag5').innerHTML = '<a href="'+tagsearch(tag05)+'/'+tagsearch(tag05)+'.html">'+tag01+'</a>';
+document.getElementById('tag5').innerHTML = '<a href="tag/'+tagsearch(tag05)+'/'+tagsearch(tag05)+'.html">'+tag01+'</a>';
 
 
 function writersearch(x){
